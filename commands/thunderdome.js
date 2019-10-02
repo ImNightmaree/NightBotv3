2b744be5aaeeb243b033d6042459bb45ad4066da
ImNightmaree <night@arcadiastudios.co.uk>
2019-10-03 00:40:39 +0100

    Let's start this shit.

---
+++ commands/thunderdome.js
@@ -0,0 +1,30 @@
+const Discord = require("discord.js")
+const config = require("../config")
+
+const challenged = message.mentions.members.first()
+
+exports.run = (client, message, args) => {
+
+	message.send("Please provide an argument. Struggling? Please refer to my help page! (" + config.prefix + "help" + ")")
+
+	const thunderdomeChallengeEmbed = new Discord.RichEmbed()
+		.setTitle("Thunderdome")
+		.setAuthor(client.user.username, client.user.avatarURL)
+		.setColor(config.embedColor)
+		.setDescription("You've challenged " + challenged + "! Will they accept the challenge..?")
+		.setFooter("Called by " + message.author.tag, message.author.avatarURL)
+		.setTimestamp()
+
+	if (args[0].contains(message.mentions)) {
+
+		message.send(thunderdomeChallengeEmbed)
+
+	}
+
+
+
+}
+
+exports.help = {
+	name:"thunderdome"
+}

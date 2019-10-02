544a57c09dc6c5f87526424b46e72537b537ea58
ImNightmaree <night@arcadiastudios.co.uk>
2019-10-02 23:46:36 +0100

    Code cleanup - removal of semicolons and irrelevant comment

--- commands/botrestart.js
+++ commands/botrestart.js
@@ -1,10 +1,10 @@
-const Discord = require("discord.js");
-const config = require("../config");
-const guildMod = "549215227514847232";
+const Discord = require("discord.js")
+const config = require("../config")
+const guildMod = "549215227514847232"
 
 exports.run = (client, message) => {
 
-	let logChannel = client.channels.get("549937681451188234");
+	let logChannel = client.channels.get("549937681451188234")
 
 	function resetNightBot(channel) {
 
@@ -12,17 +12,17 @@ exports.run = (client, message) => {
 			.setColor(config.embedColor)
 			.setDescription("I'm restarting now! See you soon!")
 			.setFooter("Called by " + message.author.tag, message.author.avatarURL)
-			.setTimestamp();
+			.setTimestamp()
 
-		if (!message.member.roles.has(guildMod)) { // Deportation Officer
+		if (!message.member.roles.has(guildMod)) {
 			message.react("⛔");
-			logChannel.send(":satellite: " + message.author.toString() + " attempted to restart the bot whilst unauthorized.");
+			logChannel.send(":satellite: " + message.author.toString() + " attempted to restart the bot whilst unauthorized.")
 			return
 		}
 
 		channel.send(rebootEmbed)
 			.then(() => client.destroy())
-			.then(() => client.login(process.env.TOKEN)) && console.log("[botrestart.js] Restart successful.");
+			.then(() => client.login(process.env.TOKEN)) && console.log("[botrestart.js] Restart successful.")
 	}
 
 	resetNightBot(message.channel)
@@ -31,4 +31,4 @@ exports.run = (client, message) => {
 
 exports.help = {
 	name:"restart"
-};
+}

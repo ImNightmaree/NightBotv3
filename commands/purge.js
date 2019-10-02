f4468b8500217790ed0cda1bb3524db5b3583dd9
ImNightmaree <night@arcadiastudios.co.uk>
2019-10-03 00:09:14 +0100

    Code cleanup - removal of semicolons

--- commands/purge.js
+++ commands/purge.js
@@ -1,30 +1,30 @@
-const Discord = require("discord.js");
-const config = require("../config");
+const Discord = require("discord.js")
+const config = require("../config")
 
 
 exports.run = async (client, message, args) => {
 
-	let logChannel = client.channels.get("549937681451188234");
-	const guildMod = "549215227514847232";
-	const purgeCount = parseInt(args[0], 10);
+	let logChannel = client.channels.get("549937681451188234")
+	const guildMod = "549215227514847232"
+	const purgeCount = parseInt(args[0], 10)
 
 	if (!message.member.roles.has(guildMod)) {
 		return message.react("⛔")
-			&& logChannel.send("📔 " + message.author.toString() + " has attempted to purge " + message.channel.toString() + " of " + purgeCount + " messages whilst unauthorized.");
+			&& logChannel.send("📔 " + message.author.toString() + " has attempted to purge " + message.channel.toString() + " of " + purgeCount + " messages whilst unauthorized.")
 
 	}
 
 	message.delete()
 
-	const toBePurged = await message.channel.fetchMessages({limit: purgeCount +1});
+	const toBePurged = await message.channel.fetchMessages({limit: purgeCount +1})
 	message.channel.bulkDelete(toBePurged)
-		.catch(error => console.log(error));
+		.catch(error => console.log(error))
 
 	const purgeCompleteEmbed = new Discord.RichEmbed()
 		.setColor(config.embedColor)
 		.setDescription("I have successfully purged " + purgeCount + " messages from " + message.channel.toString())
 		.setFooter("Called by " + message.author.tag, message.author.avatarURL)
-		.setTimestamp();
+		.setTimestamp()
 
 	message.channel.send(purgeCompleteEmbed)
 	logChannel.send("📔 " + message.author.toString() + " has successfully purged " + purgeCount + " messages from " + message.channel.toString())
@@ -33,4 +33,4 @@ exports.run = async (client, message, args) => {
 
 exports.help = {
 	name:"purge"
-};
+}

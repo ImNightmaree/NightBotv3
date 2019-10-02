1bfb9cf64fbd5f30ce579e8ed85ec84a1ffa2a72
ImNightmaree <night@arcadiastudios.co.uk>
2019-10-02 21:18:09 +0100

    Too many line breaks I think?

--- commands/help.js
+++ commands/help.js
@@ -14,19 +14,19 @@ exports.run = async (client, message) => {
 
 	const commandStaffEmbed = new Discord.RichEmbed()
 		.setColor(config.embedColor)
-		.setDescription("**" + config.prefix + "ban** @user reason - Permanently ban a user from the server.\n\n" +
-			"**" + config.prefix + "mute** @user - Mute a user from the server. Run the command again to unmute.\n\n" +
-			"**" + config.prefix + "purge** amount - Purge a certain amount of messages from the channel. *You must ensure your purging more than 2 messages, but less than 100 at a time.*\n\n" +
-			"**" + config.prefix + "uptime** - See how long the bot's been running for.\n\n" +
-			"**" + config.prefix + "ping** - See if the bot is responding properly, and what the ping is between the WebSocket and the client.\n\n" +
-			"**" + config.prefix + "restart** - Force the bot to restart. It should usually reboot in about a minute or two.\n\n")
+		.setDescription("**" + config.prefix + "ban** @user reason - Permanently ban a user from the server.\n" +
+			"**" + config.prefix + "mute** @user - Mute a user from the server. Run the command again to unmute.\n" +
+			"**" + config.prefix + "purge** amount - Purge a certain amount of messages from the channel. *You must ensure your purging more than 2 messages, but less than 100 at a time.*\n" +
+			"**" + config.prefix + "uptime** - See how long the bot's been running for.\n" +
+			"**" + config.prefix + "ping** - See if the bot is responding properly, and what the ping is between the WebSocket and the client.\n" +
+			"**" + config.prefix + "restart** - Force the bot to restart. It should usually reboot in about a minute or two.\n")
 		.setFooter("Need more help? Ask in the Discord!")
 		.setTimestamp();
 
 	const commandEmbed = new Discord.RichEmbed()
 		.setColor(config.embedColor)
-		.setDescription("**" + config.prefix + "uptime** - See how long the bot's been running for.\n\n" +
-						"**" + config.prefix + "ping** - See if the bot is responding properly, and what the ping is between the WebSocket and the client.\n\n")
+		.setDescription("**" + config.prefix + "uptime** - See how long the bot's been running for.\n" +
+						"**" + config.prefix + "ping** - See if the bot is responding properly, and what the ping is between the WebSocket and the client.\n")
 		.setFooter("Need more help? Ask in the Discord!")
 		.setTimestamp();
 

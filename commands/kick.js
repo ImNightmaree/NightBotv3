7571bb5fa33b935d23d8b36d5be30db51de369ad
ImNightmaree <night@arcadiastudios.co.uk>
2019-10-03 00:00:40 +0100

    Code cleanup - removal of semicolons

--- commands/kick.js
+++ commands/kick.js
@@ -1,17 +1,17 @@
-const Discord = require("discord.js");
-const config = require("../config");
+const Discord = require("discord.js")
+const config = require("../config")
 
 
 exports.run = async (client, message, args) => {
 
-	let logChannel = client.channels.get("549937681451188234");
-	let listChannel = client.channels.get("623488011971330058");
-	const guildMod = "549215227514847232";
-	const target = message.mentions.members.first();
+	let logChannel = client.channels.get("549937681451188234")
+	let listChannel = client.channels.get("623488011971330058")
+	const guildMod = "549215227514847232"
+	const target = message.mentions.members.first()
 
 	if (!message.member.roles.has(guildMod)) {
 		return await message.react("⛔")
-			&& logChannel.send("\n" + "📝 " + message.author.toString() + " has attempted to kick " + target + " whilst unauthorized.");
+			&& logChannel.send("\n" + "📝 " + message.author.toString() + " has attempted to kick " + target + " whilst unauthorized.")
 	}
 
 	if (!target) {
@@ -20,9 +20,9 @@ exports.run = async (client, message, args) => {
 			.setColor(config.embedColor)
 			.setDescription("I'm not able to kick this user.\n\nAre you mentioning a valid user?")
 			.setFooter("Called by " + message.author.tag, message.author.avatarURL)
-			.setTimestamp();
+			.setTimestamp()
 
-		return message.channel.send(targetNotExistEmbed);
+		return message.channel.send(targetNotExistEmbed)
 	}
 
 	if (!target.kickable) {
@@ -31,23 +31,23 @@ exports.run = async (client, message, args) => {
 			.setColor(config.embedColor)
 			.setDescription("I'm not able to kick this user.\n\nThey might have a higher role than I do, or I no longer have permissions to kick.")
 			.setFooter("Called by " + message.author.tag, message.author.avatarURL)
-			.setTimestamp();
+			.setTimestamp()
 
 		return message.channel.send(cannotKickEmbed)
-			&& logChannel.send("\n" + "📝" + message.author.toString() + " has attempted to kick " + target + " but the command failed, as the target was unkickable.");
+			&& logChannel.send("\n" + "📝" + message.author.toString() + " has attempted to kick " + target + " but the command failed, as the target was unkickable.")
 
 	}
 
-	let reason = args.slice(1).join(' ');
+	let reason = args.slice(1).join(' ')
 
-	if (!reason){
+	if (!reason) {
 
-		reason = "No reason provided. Please ask issuing staff member for details."; }
+		reason = "No reason provided. Please ask issuing staff member for details." }
 
-	await client.users.get(target.id).send("Hi there! I'm DMing you to inform you that " + message.author.username + "#" + message.author.discriminator + " has kicked you for the reason of '" + reason + "' For any questions, please contact them. You're free to rejoin whenever you feel ready.");
+	await client.users.get(target.id).send("Hi there! I'm DMing you to inform you that " + message.author.username + "#" + message.author.discriminator + " has kicked you for the reason of '" + reason + "' For any questions, please contact them. You're free to rejoin whenever you feel ready.")
 
 	await target.kick("(" + message.author.username + "#" + message.author.discriminator + "): " + reason)
-		.catch(error => console.log(error));
+		.catch(error => console.log(error))
 
 	const kickSuccessEmbed = new Discord.RichEmbed()
 		.setColor(config.embedColor)
@@ -55,10 +55,10 @@ exports.run = async (client, message, args) => {
 		.addField("User: ", target)
 		.addField("Reason: ", reason)
 		.setFooter("Called by " + message.author.tag, message.author.avatarURL)
-		.setTimestamp();
+		.setTimestamp()
 
-	message.channel.send(kickSuccessEmbed);
-	logChannel.send("\n" + "📝 " + message.author.toString() + " has successfully kicked " + target + " for the reason of '" + reason + "'");
+	message.channel.send(kickSuccessEmbed)
+	logChannel.send("\n" + "📝 " + message.author.toString() + " has successfully kicked " + target + " for the reason of '" + reason + "'")
 
 	const kickLoggingEmbed = new Discord.RichEmbed()
 		.setColor(config.embedColor)
@@ -66,11 +66,11 @@ exports.run = async (client, message, args) => {
 		.addField("User: ", target)
 		.addField("Reason: ", reason)
 		.addField("Issued by: ", message.author.toString())
-		.setTimestamp();
+		.setTimestamp()
 
 	listChannel.send(kickLoggingEmbed)
-};
+}
 
 exports.help = {
 	name:"kick"
-};
+}

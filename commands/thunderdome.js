3bfe990d2f9a5cabd3d1886b06999151f66e8d5a
ImNightmaree <night@arcadiastudios.co.uk>
2019-10-03 01:24:45 +0100

    Whoops, that didn't work!

--- commands/thunderdome.js
+++ commands/thunderdome.js
@@ -19,7 +19,7 @@ exports.run = (client, message, args) => {
 		.setFooter("Called by " + message.author.tag, message.author.avatarURL)
 		.setTimestamp()
 
-	if (args[0] === "challenge" && args[1].includes(message.mentions)) {
+	if (args[0] === "challenge" && message.mentions.members.first()) {
 
 		message.channel.send(thunderdomeChallengeEmbed)
 

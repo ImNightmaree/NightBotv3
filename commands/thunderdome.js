4f8867ec24564ed3ac653cdb291c984477641703
ImNightmaree <night@arcadiastudios.co.uk>
2019-10-03 01:45:43 +0100

    Basic functionality.

--- commands/thunderdome.js
+++ commands/thunderdome.js
@@ -3,7 +3,7 @@ const config = require("../config")
 
 
 
-exports.run = (client, message, args) => {
+exports.run = async (client, message, args) => {
 
 	const challenged = message.mentions.members.first()
 
@@ -22,11 +22,15 @@ exports.run = (client, message, args) => {
 	if (args[0] === "challenge" && message.mentions.members.first()) {
 
 		message.channel.send(thunderdomeChallengeEmbed)
-
+		await message.react("✅")
+		await message.react("❎")
 	}
 
+	const acceptFilter = (reaction, user) => reaction.emoji.name === "✅" && user.id !== config.botID
+	const denyFilter = (reaction, user) => reaction.emoji.name === "❎" && user.id !== config.botID
 
-
+	acceptFilter.on("collect", () => message.channel.send(challenged.toString + " has accepted the challenge! Let the battle commence! You can roast once every 30 seconds, and this continues until a judge decides the winning roast. With that said, begin!"))
+	denyFilter.on("collect", () => message.channel.send(challenged.toString() + " is too chicken and has bailed from the challenge... pussy. Try challenging someone else! Maybe they'll live up to the challenge..."))
 }
 
 exports.help = {

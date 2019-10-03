a43cfb4343d1a2f416572b8d6177af0556172c83
ImNightmaree <night@arcadiastudios.co.uk>
2019-10-03 22:12:25 +0100

    Let's make it so we're not locked out the entire time if we shut down.

--- index.js
+++ index.js
@@ -116,3 +116,8 @@ client.on("message", async message => {
 
 })
 client.login(config.token)
+
+process.on("exit", () => db.close())
+process.on("SIGHUP", () => process.exit(128 + 1))
+process.on("SIGINT", () => process.exit(128 + 2))
+process.on("SIGTERM", () => process.exit(128 + 15))

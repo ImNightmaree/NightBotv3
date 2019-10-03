9b0bc0f29d5e6f7e1ca1c3b2dba4091ead7d7904
ImNightmaree <night@arcadiastudios.co.uk>
2019-10-03 02:00:45 +0100

    Move it all inside of a .then catch

--- commands/thunderdome.js
+++ commands/thunderdome.js
@@ -22,18 +22,25 @@ exports.run = async (client, message, args) => {
 	if (args[0] === "challenge" && message.mentions.members.first()) {
 
 		message.channel.send(thunderdomeChallengeEmbed)
-		await message.react("✅")
-		await message.react("❎")
+			.then(async function (message) {
+
+				await message.react("✅")
+				await message.react("❎")
+
+				const acceptFilter = (reaction, user) => reaction.emoji.name === "✅" && user.id !== config.botID && user.id === challenged.id
+				const acceptCollector = message.createReactionCollector(acceptFilter)
+
+				const denyFilter = (reaction, user) => reaction.emoji.name === "❎" && user.id !== config.botID && user.id === challenged.id
+				const denyCollector = message.createReactionCollector(denyFilter)
+
+				acceptCollector.on("collect", () => message.channel.send(challenged.toString + " has accepted the challenge! Let the battle commence! You can roast once every 30 seconds, and this continues until a judge decides the winning roast. With that said, begin!"))
+				denyCollector.on("collect", () => message.channel.send(challenged.toString() + " is too chicken and has bailed from the challenge... pussy. Try challenging someone else! Maybe they'll live up to the challenge..."))
+
+			})
+
 	}
 
-	const acceptFilter = (reaction, user) => reaction.emoji.name === "✅" && user.id !== config.botID
-	const acceptCollector = message.createReactionCollector(acceptFilter)
 
-	const denyFilter = (reaction, user) => reaction.emoji.name === "❎" && user.id !== config.botID
-	const denyCollector = message.createReactionCollector(denyFilter)
-
-	acceptCollector.on("collect", () => message.channel.send(challenged.toString + " has accepted the challenge! Let the battle commence! You can roast once every 30 seconds, and this continues until a judge decides the winning roast. With that said, begin!"))
-	denyCollector.on("collect", () => message.channel.send(challenged.toString() + " is too chicken and has bailed from the challenge... pussy. Try challenging someone else! Maybe they'll live up to the challenge..."))
 }
 
 exports.help = {

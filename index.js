f90f5d71c8c669f38b2380e8767e93dbd7dabd18
ImNightmaree <night@arcadiastudios.co.uk>
2019-10-06 20:20:24 +0100

    Only open DB connection on guildMemberAdd event

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 require('dotenv').config({path: __dirname + '/.env'})
 
 const config = require("./config.js")
-const db = require("better-sqlite3")("./thunderdome.db")
 const Discord = require("discord.js")
 const fs = require("fs")
 const client = new Discord.Client()
@@ -68,6 +67,8 @@ client.on("guildCreate", guild => {
 })
 
 client.on("guildMemberAdd", member => {
+
+	const db = require("better-sqlite3")("./thunderdome.db")
 
 	console.log("[Thunderdome] A user has joined! Deleting their table (if exists) and creating a new one now!")
 	const deleteRow = db.prepare("DELETE FROM users WHERE id = ?")

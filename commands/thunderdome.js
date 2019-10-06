36c47293b4b892fcaf5f2c33b08cb61591e4d1fb
ImNightmaree <night@arcadiastudios.co.uk>
2019-10-06 21:55:43 +0100

    pls

--- commands/thunderdome.js
+++ commands/thunderdome.js
@@ -57,20 +57,20 @@ exports.run = async (client, message, args) => {
 
 	}
 
-	if (args[0] === "winner" && message.mentions.first() && !message.author.member.roles.has(judgeRole)) {
+	if (args[0] === "winner" && message.mentions.members.first() && !message.author.member.roles.has(judgeRole)) {
 		message.author.send("You can't declare a winner because you aren't a Judge.")
 	}
 
-	if (args[0] === "winner" && !message.mentions.first() && message.author.member.roles.has(judgeRole)) {
+	if (args[0] === "winner" && !message.mentions.members.first() && message.author.member.roles.has(judgeRole)) {
 		message.author.send("You haven't mentioned a user to declare winner.")
 	}
 
-	if (args[0] === "winner" && message.mentions.first() && message.author.member.roles.has(judgeRole)) {
+	if (args[0] === "winner" && message.mentions.members.first() && message.author.member.roles.has(judgeRole)) {
 		const grabPoints = db.prepare("SELECT points FROM users WHERE id = ?")
-		const resultGrab = grabPoints.all(message.mentions.first().id)
+		const resultGrab = grabPoints.all(message.mentions.members.first().id)
 
 		const updatePoints = db.prepare("UPDATE users SET points = ? WHERE id = ?")
-		updatePoints.run(resultGrab+1, message.mentions.first().id)
+		updatePoints.run(resultGrab+1, message.mentions.members.first().id)
 
 		message.react("👍")
 	}

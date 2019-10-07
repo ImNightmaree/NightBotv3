f05876329907c8eb37ec218981eb25665c0d872f
ImNightmaree <night@arcadiastudios.co.uk>
2019-10-07 21:11:26 +0100

    youtu.be is valid, so allow that to be exempt.

--- commands/music.js
+++ commands/music.js
@@ -9,7 +9,7 @@ exports.run = async (client, message, args) => {
 		if (message.guild.me.voiceChannel) { // If bot is currently in a voice channel.
 			return message.channel("I'm already in a voice channel - sadly I can't be in two places at once!\n\nPlease either disconnect me from my current channel and try again, or move me.")
 		}
-		if (!args[1] || !args[1].includes("youtube.com")) { // If there aren't any arguments for the second position, or if it doesn't contain youtube.
+		if (!args[1] || !args[1].includes("youtube.com" || !args[1].includes("youtu.be"))) { // If there aren't any arguments for the second position, or if it doesn't contain youtube.
 			return message.channel.send("You haven't provided a URL to play!\n\nPlease provide a proper URL and try again.")
 		}
 

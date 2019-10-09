327d526135b719d77837fe967265dbc0517ed643
ImNightmaree <nightdawns@protonmail.com>
2019-10-09 21:52:59 +0100

    I don't think this will work but I guess we'll see?

--- commands/music.js
+++ commands/music.js
@@ -1,13 +1,11 @@
 const ytdl = require("ytdl-core")
+const musicQueue = new Map()
 
 exports.run = async (client, message, args) => {
 
 	if (args[0] === "play") {
 		if (!message.member.voiceChannel) { // If member isn't in a voice channel.
 			return message.channel.send("I can't play music in a voice channel that you aren't currently in.\n\nPlease join a voice channel and try again.")
-		}
-		if (message.guild.me.voiceChannel) { // If bot is currently in a voice channel.
-			return message.channel("I'm already in a voice channel - sadly I can't be in two places at once!\n\nPlease either disconnect me from my current channel and try again, or move me.")
 		}
 		if (!args[1]) { // If there aren't any arguments for the second position.
 			return message.channel.send("You haven't provided a URL to play!\n\nPlease provide a proper URL and try again.")
@@ -20,10 +18,47 @@ exports.run = async (client, message, args) => {
 		}
 
 		const videoInfo = await ytdl.getInfo(args[1])
-		const connection = await message.member.voiceChannel.join()
-		const dispatcher = await connection.playStream(ytdl(args[1], { filter: "audioonly"}))
-		message.channel.send("We're now playing **" + videoInfo.title + "** requested by " + message.author.tag + " in **" + message.member.voiceChannel.name + "**!")
+		const data = musicQueue.get(message.guild.id) || {}
 
+		if (!data.connection) data.connection = await message.member.voiceChannel.join() // If we don't have a connection, let' start one.
+		if (!data.queue) data.queue = [] // If there isn't an array for our queue, then let's make one.
+		data.guildID = message.guildID
+
+		data.queue.push({ // Let's add it to the queue.
+			songTitle: videoInfo.title,
+			requester: message.author.tag,
+			url: args[1],
+			announceIn: message.channel.id
+		})
+
+		if (!data.dispatcher) await play(client, data) // If there's no dispatcher, let's make one and play the song.
+		else {
+			message.channel.send(message.author.tag + " has added " + videoInfo.title + " to the queue!")
+		}
+		musicQueue.set(message.guild.id, data)
+
+		async function play(client, data) {
+			client.channels.get(data.queue[0].announceIn.send("We're now playing " + data.queue[0].songTitle + " requested by " + data.queue[0].requester))
+			data.dispatcher = await data.connection.play(ytdl(data.queue[0].url, { filter: "audioonly" }))
+			data.dispatcher.guildID = data.guildID
+
+			data.dispatcher.once("finish", function(){
+				finish(client, this)
+			})
+		}
+
+		function finish(client, dispatcher) {
+			const fetched = musicQueue.get(dispatcher.guildID)
+			fetched.queue.shift()
+			if (fetched.queue.length > 0) { // Is the queue empty?
+				musicQueue.set(dispatcher.guildID, fetched)
+				play(client, fetched)
+			} else {
+				musicQueue.delete(dispatcher.guildID)
+				const vc = client.guilds.get(dispatcher.guildID).me.voiceChannel
+				if (vc) vc.leave()
+			}
+		}
 	}
 
 	if (args[0] === "leave") {

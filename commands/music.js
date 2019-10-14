a3e0eebd6e9fc0525db3bfccf8f46423c8aff279
ImNightmaree <nightdawns@protonmail.com>
2019-10-14 23:13:11 +0100

    Let's just remove that check entirely...

--- commands/music.js
+++ commands/music.js
@@ -7,9 +7,6 @@ exports.run = async (client, message, args, ops) => {
 		if (!message.member.voiceChannel) { // If member isn't in a voice channel.
 			return message.channel.send("I can't play music in a voice channel that you aren't currently in.\n\nPlease join a voice channel and try again.")
 		}
-		if (message.guild.me.voiceChannel) { // If bot is currently in a voice channel.
-			return message.channel.send("I'm already in a voice channel - sadly I can't be in two places at once!\n\nPlease either disconnect me from my current channel and try again, or move me.")
-		}
 		if (!args[1]) { // If there aren't any arguments for the second position.
 			return message.channel.send("You haven't provided a URL to play!\n\nPlease provide a proper URL and try again.")
 		}

7b7e22731f0876acf8c091081fad0863fe8b5676
ImNightmaree <nightdawns@protonmail.com>
2019-10-15 03:44:14 +0100

    rev.

--- commands/music.js
+++ commands/music.js
@@ -142,9 +142,9 @@ exports.run = async (client, message, args, ops) => {
 		let fetched = ops.active.get(message.guild.id)
 		if (!fetched) return message.channel.send ("Nothing is currently playing right now.")
 		if (message.member.voiceChannel !== message.guild.me.voiceChannel) return message.channel.send("You can't adjust the volume if you aren't in the same channel as me.")
-		if (isNaN[args[1]]) return message.channel.send("Our current volume is " + fetched.dispatcher.volume + "%. If you're looking to change this, run the command again using a value between 0-200.")
+		if (isNaN[args[1]]) return message.channel.send("Please provide an number between 0-200")
 		if (args[1] > 200) return message.channel.send("You've hit the max volume allowed - please go lower. We only allow values between 0-200.")
-		if (args[1] <= 0) return message.channel.send("You can't set the volume to be lower than or equal to 0.")
+		if (args[1] <= 0) return message.channel.send("You can't set the volume to be lower than 0.")
 		fetched.dispatcher.setVolume(args[1]/100)
 		message.channel.send(`Successfully set the volume of **${fetched.queue[0].title}** to ${args[1]}`)
 	}
